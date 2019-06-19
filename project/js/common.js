2f3bc7cb0c60c5313dcdfb0c9444243d69091a5b
Lensom <magurawork@gmail.com>
2019-06-19 16:56:32 +0300

    add one more page and media to faq

--- project/js/common.js
+++ project/js/common.js
@@ -1,6 +1,40 @@
 $(document).ready(function() {
 
-  
+  // const sidebarItem = $('.dropdown-item'); 
+  // const menu = $('.menu');
+  // menu.addClass('menu-none');
+
+  // sidebarItem.on('click', function(e){
+  //   if ($(menu).not('.menu-none')) {
+  //       menu.css('display', 'none');
+  //   }
+  //   $(this).find(menu).toggleClass('menu-none');
+  //   $(this).find(menu).css('display', 'block');
+    
+  // });
+
+  $('.switch').on('click', function(){
+    $('.switch').removeClass('act');
+    $(this).toggleClass('act');
+    $('.auto-item').removeClass('current');
+    $(this).parent().parent('.auto-item').addClass('current');
+  });
+
+  $( function() {
+    $( "#slider-range-min" ).slider({
+      range: "min",
+      value: 60,
+      min: 0,
+      max: 100,
+      slide: function( event, ui ) {
+        $( "#amount" ).text(  ui.value );
+        $( "#amount2" ).text( 100 - ui.value );
+      }
+    });
+    //$( "#amount" ).text( $( "#slider-range-min" ).slider( "value" ) );
+  });
+
+
 
 });
 
