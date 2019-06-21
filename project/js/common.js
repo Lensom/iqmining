fbb2c4e77c284a17e5989731937e1a915625643e
Lensom <magurawork@gmail.com>
2019-06-21 11:28:40 +0300

    add full third page with modal

--- project/js/common.js
+++ project/js/common.js
@@ -147,8 +147,70 @@ $('.auto-amount-input').keypress(function (e) {
   $(function() {
 
     $('.ref-select').styler();
+
+    $('.modal-select').styler();
   
   });
+
+  // Get the modal
+  var modal = document.getElementById("transferModal");
+  var modal2 = document.getElementById("linkModal");
+  var modal3 = document.getElementById("watchModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("transfer");
+  var btn2 = document.getElementById("table-button");
+  var btn3 = document.getElementById("modal-watch");
+
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  var span2 = document.getElementsByClassName("close")[1];
+  var span3 = document.getElementsByClassName("close")[2];
+
+  // When the user clicks on the button, open the modal 
+  btn.onclick = function(e) {
+    e.preventDefault();
+    modal.style.display = "block";
+  }
+  btn2.onclick = function(e) {
+    e.preventDefault();
+    modal2.style.display = "block";
+  }
+  btn3.onclick = function(e) {
+    e.preventDefault();
+    modal3.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  span2.onclick = function() {
+    modal2.style.display = "none";
+  }
+  span3.onclick = function() {
+    modal3.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+    if (event.target == modal2) {
+      modal2.style.display = "none";
+    }
+    if (event.target == modal3) {
+      modal3.style.display = "none";
+    }
+  }
+
+  $('.modal-choose').on('click', function(){
+    $('.modal-choose').removeClass('modal-current');
+    $(this).addClass('modal-current');
+  });
+
 
 });
 
